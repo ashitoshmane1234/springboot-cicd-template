cbc24dae31ca187e3d7c189de26b0a82bcbd4d6e
Ashitosh Mane <ashitosh.mane@mindbowser.com>
2025-12-23 21:03:21 +0530

    Initialize Spring Boot project

---
+++ src/main/java/com/ashitosh/cicd/SpringbootCicdTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ashitosh.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicdTemplateApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ashitosh/cicd/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.ashitosh.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "CI/CD Pipeline is working!";
+    }
+}
